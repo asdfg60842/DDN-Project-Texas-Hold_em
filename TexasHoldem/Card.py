acbf1c65f6699b4628e3395f242fd71e55a62b1b
orion_gz <asdfg60842@gmail.com>
2023-07-22 21:02:50 +0900

    merge with bnghn

--- TexasHoldem/Card.py
+++ TexasHoldem/Card.py
@@ -7,9 +7,6 @@ class Community:
 
     def init_community_cards(self):
         self.__community_cards = []
-
-    def init_community_cards(self):
-        self.community_cards = []
         
     def add_card(self, aCard):
         self.__community_cards.append(aCard)
