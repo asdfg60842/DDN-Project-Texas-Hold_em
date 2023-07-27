71cabedf639bba5cba083a4d0c65c30626917002
orion_gz <asdfg60842@gmail.com>
2023-07-27 16:43:44 +0900

    Structure Build

--- TexasHoldem/Card.py
+++ TexasHoldem/Card.py
@@ -60,3 +60,126 @@ class Deck:
     
     def burn_card(self):
         self.deck_cards.pop()
+        
+class ShowDown:
+    def __init__(self, hand, community_cards):
+        self.whole_cards = []
+        self.ranking = []
+        
+        self.whole_cards.append(hand)
+        self.whole_cards.append(community_cards)
+        self.whole_cards.sort(key = lambda own_card: own_card.rank)
+        
+    def show_down(self):
+        # 족보 판단하는 변수 - 해당 족보의 갯수와 숫자를 저장 ex) 7, 5 투페어: self.is_pair = [2, 7, 5]
+        self.self.is_straightflush = [0]
+        self.is_fourcard = [0]
+        self.is_flush = [0]
+        self.is_straight = [0]
+        self.is_triple = [0]
+        self.is_pair = [0]
+
+        self.straightflush_flush_func(self.whole_cards)
+        self.straight_func(self.whole_cards)
+        self.pair_triple_fourcard_func(self.whole_cards)
+
+        # return ranking
+        # straight flush : 8
+        # four card : 7
+        # full house : 6
+        # flush : 5
+        # straight : 4
+        # triple : 3
+        # two pair : 2
+        # one pair : 1
+        # top : 0
+        if self.self.is_straightflush[0] == 1:
+            self.ranking = [8, self.self.is_straightflush[1]]
+        elif self.is_fourcard[0] == 1:
+            self.ranking = [7, self.is_fourcard[1], self.is_fourcard[2]]
+        elif self.is_triple[0] == 1 and self.is_pair[0] >= 1:
+            self.ranking = [6, self.is_triple[1], self.is_pair[1]]
+        elif self.is_triple[0] == 2:
+            self.ranking = [6, self.is_triple[1], self.is_triple[2]]
+        elif self.is_flush[0] == 1:
+            self.ranking = [5, self.is_flush[1]]
+        elif self.is_straight[0] == 1:
+            self.ranking = [4, self.is_straight[1]]
+        elif self.is_triple[0] == 1:
+            self.ranking = [3, self.is_triple[1], self.is_triple[2]]
+        elif self.is_pair[0] == 3:
+            self.ranking = [2, self.is_pair[1], self.is_pair[2], self.is_pair[3]]
+        elif self.is_pair[0] == 2:
+            self.ranking = [2, self.is_pair[1], self.is_pair[2], self.is_pair[3]]
+        elif self.is_pair[0] == 1:
+            self.ranking = [1, self.is_pair[1], self.is_pair[2], self.is_pair[3], self.is_pair[4]]
+        else:
+            self.ranking = [0]
+            for i in range(6, 1, -1):
+                self.ranking.append(self.whole_cards[i].rank)
+                
+    # 스티플, 플러쉬 판단
+    def straightflush_flush_func(self, card_list):
+        # 문양 갯수 세는 변수
+        suit_num = [0, 0, 0, 0]
+        for i in range(7):
+            suit_num[card_list[i].suit] += 1
+        for i in range(4):
+            if suit_num[i] >= 5: self.is_flush[0] = 1
+            flush_list = []
+            for j in range(7):
+                if card_list[j].suit == i: flush_list.append(card_list[j])
+            self.straight_func(flush_list)
+            if self.is_straight[0] == 1:
+                self.self.is_straightflush = self.is_straight
+                self.self.is_straightflush[0] = 1
+            for j in range(len(flush_list) - 1, -1, -1):
+                self.is_flush.append(flush_list[j].rank)
+                
+    # 스트레이트 판단
+    def straight_func(self, card_list):
+        continuity = 1
+        for i in range(7):
+            if card_list[i].rank == card_list[i - 1].rank + 1: continuity += 1
+            elif card_list[i].rank != card_list[i - 1].rank: continuity = 1
+            if continuity >= 5:
+                self.is_straight[0] = 1
+                self.is_straight[1] = card_list[i].rank
+                for j in range(1, 5):
+                    self.is_straight.append(card_list[i - j].rank)
+                    
+    # 페어, 트리플, 포카드 판단
+    def pair_triple_fourcard_func(self, card_list):
+        counter = {}
+        whole_list = []
+        for i in range(7):
+            whole_list.append(card_list[i].rank)
+        for num in whole_list:
+            try: counter[num] += 1
+            except: counter[num] = 1
+        for num in counter.keys:
+            if counter[num] == 2:
+                self.is_pair[0] += 1
+                self.is_pair.insert(1, num)
+            if counter[num] == 3:
+                self.is_triple[0] += 1
+                self.is_triple.insert(1, num)
+            if counter[num] == 4:
+                self.is_fourcard[0] += 1
+                self.is_fourcard.append(num)
+        if self.is_pair[0] == 1:
+            for i in range(6, -1, -1):
+                if card_list[i].rank != self.is_pair[1]:
+                    self.is_pair.append(card_list[i])
+        if self.is_pair[0] == 2:
+            for i in range(6, -1, -1):
+                if card_list[i].rank != self.is_pair[1] and card_list[i].rank != self.is_pair[2]:
+                    self.is_pair.append(card_list[i])
+        if self.is_triple[0] == 1:
+            for i in range(6, -1, -1):
+                if card_list[i].rank != self.is_triple[1]:
+                    self.is_triple.append(card_list[i])
+        if self.is_fourcard[0] == 1:
+            for i in range(6, -1, -1):
+                if card_list[i].rank != self.is_fourcard[1]:
+                    self.is_fourcard.append(card_list[i])
--- TexasHoldem/Player.py
+++ TexasHoldem/Player.py
@@ -10,8 +10,6 @@ class Player():
         self.bet_status = None
         self.round_result = None
         self.betting_history = []
-        self.whole_cards = []
-        self.ranking = []
 
     def init_hand(self):
         self.hand = []
@@ -27,143 +25,17 @@ class Player():
     def add_card(self, aCard):
         self.hand.append(aCard)
 
-    def show_down(self, community_cards):
-        self.whole_cards.append(self.__hand)
-        self.whole_cards.append(community_cards)
-        self.whole_cards.sort(key = lambda own_card: own_card.rank)
-
-        # 족보 판단하는 변수 - 해당 족보의 갯수와 숫자를 저장 ex) 7, 5 투페어: is_pair = [2, 7, 5]
-        is_straightflush = [0]
-        is_fourcard = [0]
-        is_flush = [0]
-        is_straight = [0]
-        is_triple = [0]
-        is_pair = [0]
-
-        # 스티플, 플러쉬 판단
-        def straightflush_flush_func(card_list):
-            # 문양 갯수 세는 변수
-            suit_num = [0, 0, 0, 0]
-
-            for i in range(7):
-                suit_num[card_list[i].suit] += 1
-            for i in range(4):
-                if suit_num[i] >= 5: is_flush[0] = 1
-
-                flush_list = []
-                for j in range(7):
-                    if card_list[j].suit == i: flush_list.append(card_list[j])
-                straight_func(flush_list)
-                if is_straight[0] == 1:
-                    is_straightflush = is_straight
-                    is_straightflush[0] = 1
-
-                for j in range(len(flush_list) - 1, -1, -1):
-                    is_flush.append(flush_list[j].rank)
-
-        # 스트레이트 판단
-        def straight_func(card_list):
-            continuity = 1
-
-            for i in range(7):
-                if card_list[i].rank == card_list[i - 1].rank + 1: continuity += 1
-                elif card_list[i].rank != card_list[i - 1].rank: continuity = 1
-
-                if continuity >= 5:
-                    is_straight[0] = 1
-                    is_straight[1] = card_list[i].rank
-                    for j in range(1, 5):
-                        is_straight.append(card_list[i - j].rank)
-
-        # 페어, 트리플, 포카드 판단
-        def pair_triple_fourcard_func(card_list):
-            counter = {}
-            whole_list = []
-            for i in range(7):
-                whole_list.append(card_list[i].rank)
-
-            for num in whole_list:
-                try: counter[num] += 1
-                except: counter[num] = 1
-
-            for num in counter.keys:
-                if counter[num] == 2:
-                    is_pair[0] += 1
-                    is_pair.insert(1, num)
-                if counter[num] == 3:
-                    is_triple[0] += 1
-                    is_triple.insert(1, num)
-                if counter[num] == 4:
-                    is_fourcard[0] += 1
-                    is_fourcard.append(num)
-
-            if is_pair[0] == 1:
-                for i in range(6, -1, -1):
-                    if card_list[i].rank != is_pair[1]:
-                        is_pair.append(card_list[i])
-            if is_pair[0] == 2:
-                for i in range(6, -1, -1):
-                    if card_list[i].rank != is_pair[1] and card_list[i].rank != is_pair[2]:
-                        is_pair.append(card_list[i])
-            if is_triple[0] == 1:
-                for i in range(6, -1, -1):
-                    if card_list[i].rank != is_triple[1]:
-                        is_triple.append(card_list[i])
-            if is_fourcard[0] == 1:
-                for i in range(6, -1, -1):
-                    if card_list[i].rank != is_fourcard[1]:
-                        is_fourcard.append(card_list[i])
-
-        straightflush_flush_func(self.whole_cards)
-        straight_func(self.whole_cards)
-        pair_triple_fourcard_func(self.whole_cards)
-
-        # return ranking
-        # straight flush : 8
-        # four card : 7
-        # full house : 6
-        # flush : 5
-        # straight : 4
-        # triple : 3
-        # two pair : 2
-        # one pair : 1
-        # top : 0
-        if is_straightflush[0] == 1:
-            self.ranking = [8, is_straightflush[1]]
-        elif is_fourcard[0] == 1:
-            self.ranking = [7, is_fourcard[1], is_fourcard[2]]
-        elif is_triple[0] == 1 and is_pair[0] >= 1:
-            self.ranking = [6, is_triple[1], is_pair[1]]
-        elif is_triple[0] == 2:
-            self.ranking = [6, is_triple[1], is_triple[2]]
-        elif is_flush[0] == 1:
-            self.ranking = [5, is_flush[1]]
-        elif is_straight[0] == 1:
-            self.ranking = [4, is_straight[1]]
-        elif is_triple[0] == 1:
-            self.ranking = [3, is_triple[1], is_triple[2]]
-        elif is_pair[0] == 3:
-            self.ranking = [2, is_pair[1], is_pair[2], is_pair[3]]
-        elif is_pair[0] == 2:
-            self.ranking = [2, is_pair[1], is_pair[2], is_pair[3]]
-        elif is_pair[0] == 1:
-            self.ranking = [1, is_pair[1], is_pair[2], is_pair[3], is_pair[4]]
-        else:
-            self.ranking = [0]
-            for i in range(6, 1, -1):
-                self.ranking.append(self.whole_cards[i].rank)
-
-    def select_bet(self, game_state = None, std_bet_select = None, again = False):
-        """ Player 의 배팅 선택 함수 """
+    def action_bet(self, game_state = None, std_bet_action = None, again = False):
+        """ Player 의 베팅 선택 함수 """
         # 임시 출력(결과 확인을 위함)
         print()
         if again == True:
-            print("누군가 Raise 하여 다시 배팅합니다.")
+            print("누군가 Raise 하여 다시 베팅합니다.")
         print(self.betting_history)
-        print("{} : {} 님의 배팅 순서입니다.".format(game_state, self.name))
+        print("{} : {} 님의 베팅 순서입니다.".format(game_state, self.name))
         print("HAND : {} {}, {} {}".format(self.hand[0].suit, self.hand[0].rank, self.hand[1].suit, self.hand[1].rank))
         
-        # 플레이어의 이전 선택지가 Fold 또는 All-In일 경우 배팅을 진행할 필요가 없기 때문에 None 리턴
+        # 플레이어의 이전 선택지가 Fold 또는 All-In일 경우 베팅을 진행할 필요가 없기 때문에 None 리턴
         if self.bet_status == "Fold":
             print("플레이어가 Fold 하여 다음 순서로 넘어갑니다.")
             return None
@@ -171,17 +43,17 @@ class Player():
             print("플레이어가 All-In 하여 다음 순서로 넘어갑니다.")
             return "All-In"
         
-        # std_bet_select에 의해 선택지가 분기됨
+        # std_bet_action에 의해 선택지가 분기됨
         # 이 변수는 Game.py의 Game().bet_in_order()에 의해 기준값이 전달됨
-        if std_bet_select == "Raise" or std_bet_select == "Bet":
-            select = int(input("배팅하세요 : 1. Call\t2. Bet\t3. All-In\t4. Fold : "))
-            return self.set_bet_status(bet = std_bet_select, val = select)
-        elif std_bet_select == "Call":
-            select = int(input("배팅하세요 : 1. Call\t2. Raise\t3. All-In\t4. Fold : "))
-            return self.set_bet_status(bet = std_bet_select, val = select)
-        elif std_bet_select == "Check":
-            select = int(input("배팅하세요 : 1. Check\t2. Raise\t3. All-In\t4. Fold : "))
-            return self.set_bet_status(bet = std_bet_select, val = select)
+        if std_bet_action == "Raise" or std_bet_action == "Bet":
+            select = int(input("베팅하세요 : 1. Call\t2. Bet\t3. All-In\t4. Fold : "))
+            return self.set_bet_status(bet = std_bet_action, val = select)
+        elif std_bet_action == "Call":
+            select = int(input("베팅하세요 : 1. Call\t2. Raise\t3. All-In\t4. Fold : "))
+            return self.set_bet_status(bet = std_bet_action, val = select)
+        elif std_bet_action == "Check":
+            select = int(input("베팅하세요 : 1. Check\t2. Raise\t3. All-In\t4. Fold : "))
+            return self.set_bet_status(bet = std_bet_action, val = select)
         print()
     
     def set_bet_status(self, bet, val):
@@ -212,8 +84,8 @@ class Player():
             return "Fold"
         
     def bet(self, blind_bet = None, game_state = None, min_amount = None, again = False):
-        """ Player 의 배팅 함수 """
-        # 배팅의 기본값은 블라인드 값으로 설정
+        """ Player 의 베팅 함수 """
+        # 베팅의 기본값은 블라인드 값으로 설정
         if min_amount == None:
             min_amount = blind_bet
     
@@ -241,16 +113,16 @@ class Player():
             return min_amount
         elif self.bet_status == "Raise" or self.bet_status == "Bet":
             if self.position != 0 and game_state == "preflop":
-                bet_amount = int(input("배팅할 금액을 입력하세요(최소 금액 {} 초과) : ".format(min_amount)))
+                bet_amount = int(input("베팅할 금액을 입력하세요(최소 금액 {} 초과) : ".format(min_amount)))
                 while bet_amount < min_amount:
-                    bet_amount = int(input("배팅할 금액은 {} 초과되어야 합니다. 다시 입력해주세요 : ".format(min_amount)))
+                    bet_amount = int(input("베팅할 금액은 {} 초과되어야 합니다. 다시 입력해주세요 : ".format(min_amount)))
                 diff = bet_amount - self.position
                 self.betting_history.append([game_state, self.bet_status, diff])
                 self.current_money -= diff
             else:   
-                bet_amount = int(input("배팅할 금액을 입력하세요(최소 금액 {} 초과) : ".format(min_amount)))
+                bet_amount = int(input("베팅할 금액을 입력하세요(최소 금액 {} 초과) : ".format(min_amount)))
                 while bet_amount < min_amount:
-                    bet_amount = int(input("배팅할 금액은 {} 초과되어야 합니다. 다시 입력해주세요 : ".format(min_amount)))
+                    bet_amount = int(input("베팅할 금액은 {} 초과되어야 합니다. 다시 입력해주세요 : ".format(min_amount)))
                 self.betting_history.append([game_state, self.bet_status, bet_amount])
                 self.current_money -= bet_amount
                     
@@ -276,9 +148,9 @@ class Player():
             self.current_money -= diff
             return min_amount
         elif self.bet_status == "Raise" or self.bet_status == "Bet":
-            bet_amount = int(input("배팅할 금액을 입력하세요(최소 금액 {} 초과) : ".format(min_amount)))
+            bet_amount = int(input("베팅할 금액을 입력하세요(최소 금액 {} 초과) : ".format(min_amount)))
             while bet_amount < min_amount:
-                bet_amount = int(input("배팅할 금액은 {} 초과되어야 합니다. 다시 입력해주세요 : ".format(min_amount)))
+                bet_amount = int(input("베팅할 금액은 {} 초과되어야 합니다. 다시 입력해주세요 : ".format(min_amount)))
                 
             diff = bet_amount
             for state, _, amount in self.betting_history:
@@ -310,7 +182,7 @@ class ComputerAI(Player):
         super().__init__(name, money)
 
     def bet(self, blind_bet = None, game_state = None):
-        """ 컴퓨터 AI 의 배팅 함수 """
+        """ 컴퓨터 AI 의 베팅 함수 """
         if game_state == "blind":
             self.betting_history.append(blind_bet)
             self.current_money -= blind_bet
